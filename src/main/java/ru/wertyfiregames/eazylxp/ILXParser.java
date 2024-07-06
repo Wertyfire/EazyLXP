9785071cbdc90fb00daead6543de0de1f8a94bd0
Wertyfire <sw405@yandex.ru>
2024-07-06 23:25:05 +0300

    Added interface ILXParser.java
    Signed-off-by: Wertyfire <sw405@yandex.ru>

--- src/main/java/ru/wertyfiregames/eazylxp/ILXParser.java
+++ src/main/java/ru/wertyfiregames/eazylxp/ILXParser.java
@@ -4,7 +4,24 @@
 
 package ru.wertyfiregames.eazylxp;
 
+/**
+ * Implementing this interface means creating new parser for LX. This is used in {@link LocalizationFile} to
+ * parse multiply extensions (.lxc for default localization and .lxp for script-like translations).
+ * @since 1.0
+ * @author Wertyfire
+ * */
 public interface ILXParser {
+    /**
+     * Method returns value of key in localization file in current language.
+     * @return value of {@code key} from localization according to current language setted by {@link #setLanguage(String)}.
+     * @param key key of translation in localization file.
+     * @since 1.0
+     * */
     String get(String key);
+    /**
+     * Set current language to {@code newLanguage}.
+     * @param newLanguage new language.
+     * @since 1.0
+     * */
     void setLanguage(String newLanguage);
 }
