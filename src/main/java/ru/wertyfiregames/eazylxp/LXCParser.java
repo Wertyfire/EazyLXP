d6c9c10eb319fc483f2cf0fbb1feece7f53e1e34
Wertyfire <sw405@yandex.ru>
2024-07-06 23:25:57 +0300

    Added parser for lxc files
    Signed-off-by: Wertyfire <sw405@yandex.ru>

--- src/main/java/ru/wertyfiregames/eazylxp/LXCParser.java
+++ src/main/java/ru/wertyfiregames/eazylxp/LXCParser.java
@@ -10,13 +10,57 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.function.BiConsumer;
 
+/**
+ * Parser for .lxc and .lexc (the same) files.
+ * @since 1.0
+ * @author Wertyfire
+ * */
 public class LXCParser implements ILXParser {
+    /**
+     * {@code File} object of lxc file.
+     * @since 1.0
+     * */
     private final File lxc;
+    /**
+     * Current language. Can be changed in constructor or by calling method
+     * {@link #setLanguage(String)}.
+     * @since 1.0
+     * @see #setLanguage(String)
+     * */
     private String language;
+    public final int LATEST_LXC_VERSION = 2;
+    /**
+     * Translations from file. Map{String key, Map{String language, String value}}
+     * @since 1.0
+     * @see #languagesIndex
+     * */
     private final Map<String, Map<String, String>> translations = new HashMap<>();
+    /**
+     * Indexes of language from <em>header</em> of lxc (See package info for more info).
+     * @since 1.0
+     * @see #translations
+     * */
     private final Map<String, Integer> languagesIndex = new HashMap<>();
+
+    /**
+     * Private constructor so don't can instantiate empty class.
+     * @since 1.0
+     * @see #LXCParser(File, String)
+     * */
+    @SuppressWarnings("unused")
+    private LXCParser() {
+        lxc = null;
+    }
+
+    /**
+     * Default constructor. Protected so instantiate can only from {@link LocalizationFile}. Set {@link #lxc}
+     * to {@code file}, {@link #language} to {@code defaultLanguage} and call {@link #processStrings()}
+     * @param file file from .lxc.
+     * @param defaultLanguage default language. To change use {@link #setLanguage(String)}
+     * @since 1.0
+     * @see #LXCParser()
+     * */
     protected LXCParser(File file, String defaultLanguage) {
         lxc = file;
 
@@ -29,11 +73,23 @@ public class LXCParser implements ILXParser {
         }
     }
 
+    /**
+     * Parse all lines in localization file. If syntax invalid, throws {@code LXCSyntaxException}.
+     * @since 1.0
+     * @throws LXCSyntaxException if lxc syntax is invalid.
+     * */
     private void processStrings() throws LXCSyntaxException {
         try (BufferedReader reader = new BufferedReader(new FileReader(lxc))) {
+            String v = reader.readLine();
+            if (!v.startsWith("!version "))
+                throw new LXCSyntaxException("Invalid syntax of lxc. File not starts with !version");
+            int version = Integer.parseInt(v.substring(v.indexOf("n") + 2));
+            if (version > 0 && version <= LATEST_LXC_VERSION - 1)
+                throw new LXCSyntaxException("You trying to parse lxc file using version " + version + ", but last supported version is 2. Make sure that you entered correct version or update library to latest version.");
+
             String header = reader.readLine();
             if (header == null)
-                throw new LXCSyntaxException("XLC header is null!");
+                throw new LXCSyntaxException("LXC header is null!");
 
             String[] headerParts = header.split("\\|");
             for (int i = 1; i < headerParts.length; i++) {
@@ -41,7 +97,16 @@ public class LXCParser implements ILXParser {
             }
 
             String line;
+
             while ((line = reader.readLine()) != null) {
+                if (line.contains("#")) {
+                    if (line.startsWith("#"))
+                        continue;
+                    else {
+                        String[] translation = line.split("#");
+                        line = translation[0];
+                    }
+                }
                 String[] parts = line.split("\\|");
                 if (parts.length != headerParts.length)
                     throw new LXCSyntaxException("Line '" + line + "' format does not equals to header '" + header + "'.");
@@ -54,14 +119,34 @@ public class LXCParser implements ILXParser {
                 translations.put(key, translationMap);
             }
         } catch (IOException e) {
-            throw new RuntimeException("Could not read lxc file! Error:\n" + e.getMessage());
+            throw new RuntimeException("Could not read lxc file! Error: " + e.getMessage());
         }
     }
 
+    /**
+     * Set current language to {@code newLanguage}.
+     * @param newLanguage new language.
+     * */
     public void setLanguage(String newLanguage) {
         language = newLanguage;
     }
 
+    /**
+     * Get index of language {@code language} from <em>header</em>. Can be used for debug.
+     * @return index of {@code language}.
+     * @param language language name from <em>header</em>.
+     * @see #languagesIndex
+     * */
+    public int getLanguageIndex(String language) {
+        return languagesIndex.get(language);
+    }
+
+    /**
+     * Get value of {@code key} from file.
+     * @return value of{@code key}.
+     * @param key key of translation from file.
+     * @see #translations
+     * */
     public String get(String key) {
         Map<String, String> translationsMap = translations.get(key);
         if (translationsMap != null) {
--- src/main/java/ru/wertyfiregames/eazylxp/LXCSyntaxException.java
+++ src/main/java/ru/wertyfiregames/eazylxp/LXCSyntaxException.java
@@ -4,12 +4,31 @@
 
 package ru.wertyfiregames.eazylxp;
 
+import java.io.Serial;
+
+/**
+ * Thrown from {@link LXCParser} to indicate that parsing {@code lxc file} has invalid syntax.
+ * @since 1.0
+ * @author Wertyfire
+ * */
 public class LXCSyntaxException extends Exception {
+    @Serial
+    private static final long serialVersionUID = -4249599111868885875L;
+
+    /**
+     * Constructs a {@code LXCSyntaxException} with no
+     * detail message.
+     * */
     public LXCSyntaxException() {
         super();
     }
 
-    public LXCSyntaxException(String message) {
-        super(message);
+    /**
+     * Constructs a {@code LXCSyntaxException} with the
+     * specified detail message.
+     * @param s the detail message.
+     * */
+    public LXCSyntaxException(String s) {
+        super(s);
     }
 }
